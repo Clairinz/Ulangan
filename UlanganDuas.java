dee074c1f55ef225f1975cc88b31045641a7eb21
I_NotPro <51590857+Clairinz@users.noreply.github.com>
2020-09-08 13:51:06 +0700

    Add files via upload

---
+++ UlanganDuas.java
@@ -0,0 +1,14 @@
+package newpackage;
+/**
+ *
+ * @author Daus
+ */
+public class UlanganDuas {
+    public static void main(String args[]){
+	String str = new String( "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-");
+	System.out.print("Menggunakan String replaceall: " );
+	System.out.println(str.replaceAll("ABCDEFGHIJKLMNOPQRSTUVWXYZ#-","NUR-FIRDAUS")
+        );
+		
+   }
+}
